0c51de58a6715a077597c8ecf652c25173b79c78
44Mozart <48946383+44Mozart@users.noreply.github.com>
2019-03-26 10:32:45 +0000

    Create 045.hs

---
+++ 045.hs
@@ -0,0 +1,6 @@
+caminho :: (Int,Int) -> (Int,Int) -> [Movimento]
+caminho (x,y) (a,b) | (a > x) = Oeste : caminho (x+1,y) (a,b)
+                    | (a < x) = Este  : caminho (x-1,y) (a,b)
+                    | (b > y) = Norte : caminho (x,y+1) (a,b)
+                    | (b < y) = Sul : caminho (x,y-1) (a,b)
+                    | otherwise = []
