6ffdaa18790ae81f83dc584e01aa236ffff859cf
44Mozart <48946383+44Mozart@users.noreply.github.com>
2019-03-26 10:33:35 +0000

    Update 045.hs

--- 045.hs
+++ 045.hs
@@ -1,3 +1,6 @@
+data Movimento = Norte | Sul | Este | Oeste
+               deriving Show
+
 caminho :: (Int,Int) -> (Int,Int) -> [Movimento]
 caminho (x,y) (a,b) | (a > x) = Oeste : caminho (x+1,y) (a,b)
                     | (a < x) = Este  : caminho (x-1,y) (a,b)
